6c8eda051599a9612a69dcaad4eaaa380cfb7002
Hedström Gustav <gustavhedstrom6@gmail.com>
2017-12-04 16:01:37 +0100

    dsssad

---
+++ Advent/2017/AdventOfCode/AdventOfCode/Day5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day5
+    {
+        public Day5()
+        {
+
+        }
+
+        public void Run()
+        {
+            PuzzelOne();
+            //PuzzelTwo();
+        }
+
+        private void PuzzelOne()
+        {
+            //string[] input = File.ReadAllLines(@"C:\VS\5.txt");
+            string input = File.ReadAllText(@"C:\VS\5.txt");
+        }
+
+        private void PuzzelTwo()
+        {
+
+        }
+    }
+}
--- Advent/2017/AdventOfCode/AdventOfCode/Program.cs
+++ Advent/2017/AdventOfCode/AdventOfCode/Program.cs
@@ -14,11 +14,13 @@ namespace AdventOfCode
             Day2 day2 = new Day2();
             Day3 day3 = new Day3();
             Day4 day4 = new Day4();
+            Day5 day5 = new Day5();
 
             //day1.Run();
             //day2.Run();
-            day3.Run();
+            //day3.Run();
             //day4.Run();
+            day5.Run();
         }
     }
 }
