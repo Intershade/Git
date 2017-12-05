c5b6ce007271f895e3bf595d3d57dfee2644fcc0
Hedström Gustav <gustavhedstrom6@gmail.com>
2017-12-05 08:29:09 +0100

    asdasd

--- Advent/2017/AdventOfCode/AdventOfCode/Day5.cs
+++ Advent/2017/AdventOfCode/AdventOfCode/Day5.cs
@@ -16,19 +16,140 @@ namespace AdventOfCode
 
         public void Run()
         {
-            PuzzelOne();
-            //PuzzelTwo();
+            //PuzzelOne();
+            PuzzelTwo();
         }
 
         private void PuzzelOne()
         {
-            //string[] input = File.ReadAllLines(@"C:\VS\5.txt");
-            string input = File.ReadAllText(@"C:\VS\5.txt");
+            string[] input = File.ReadAllLines(@"C:\VS\5.txt");
+            List<int> list = new List<int>();
+            bool done = false;
+            int index = 0;
+            int prevIndex = 0;
+            int steps = 0;
+
+            foreach (var line in input.ToList())
+            {
+                if (line.Contains("-"))
+                {
+                    list.Add(-Int32.Parse(line.Replace("-", "")));
+                }
+                else
+                {
+                    list.Add(Int32.Parse(line));
+                }
+            }
+
+            do
+            {
+                if (index >= 1074)
+                {
+                    done = true;
+                }
+                else
+                {
+                    if (list[index] == 0)
+                    {
+                        list[index]++;
+                        steps++;
+                        continue;
+                    }
+                    if (list[index] > 0)
+                    {
+                        prevIndex = index;
+                        index += list[index];
+                        list[prevIndex]++;
+                        steps++;
+                        continue;
+                    }
+                    if (list[index] < 0)
+                    {
+                        prevIndex = index;
+                        index += list[index];
+                        list[prevIndex]++;
+                        steps++;
+                        continue;
+                    }
+                }
+                
+            } while (!done);
+
+            string stop = null;
         }
 
         private void PuzzelTwo()
         {
+            string[] input = File.ReadAllLines(@"C:\VS\5.txt");
+            List<int> list = new List<int>();
+            bool done = false;
+            int index = 0;
+            int prevIndex = 0;
+            int steps = 0;
 
+            foreach (var line in input.ToList())
+            {
+                if (line.Contains("-"))
+                {
+                    list.Add(-Int32.Parse(line.Replace("-", "")));
+                }
+                else
+                {
+                    list.Add(Int32.Parse(line));
+                }
+            }
+
+            do
+            {
+                if (index >= 1074)
+                {
+                    done = true;
+                }
+                else
+                {
+                    if (list[index] == 0)
+                    {
+                        list[index]++;
+                        steps++;
+                        continue;
+                    }
+                    if (list[index] > 0)
+                    {
+                        prevIndex = index;
+                        index += list[index];
+
+                        if (list[prevIndex] >= 3)
+                        {
+                            list[prevIndex]--;
+                        }
+                        else
+                        {
+                            list[prevIndex]++;
+                        }
+                        steps++;
+                        continue;
+                    }
+                    if (list[index] < 0)
+                    {
+                        prevIndex = index;
+                        index += list[index];
+
+                        if (list[prevIndex] >= 3)
+                        {
+                            list[prevIndex]--;
+                        }
+                        else
+                        {
+                            list[prevIndex]++;
+                        }
+                        steps++;
+                        continue;
+                    }
+                }
+
+            } while (!done);
+
+            string stop = null;
         }
     }
 }
